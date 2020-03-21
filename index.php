71418bcad6168e17e7a64945e4ea7848d2c83a7f
always-info <alwaysinfo291102@gmail.com>
2020-03-21 21:03:41 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
 	<h1>hello world!</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi quasi culpa officiis esse eos magnam nesciunt aperiam, iusto tenetur, quas harum ducimus est odio, eligendi natus, doloribus tempore qui architecto.</p>
 </body>
 </html>
